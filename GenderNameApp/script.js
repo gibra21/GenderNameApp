91840308c33f968524e6936d080433c19f933074
gibra21 <128503546+gibra21@users.noreply.github.com>
2023-10-12 12:02:08 -0400

    Add files via upload
    Languages used: Javascript, HTML and CSS

---
+++ GenderNameApp/script.js
@@ -0,0 +1,29 @@
+let details = document.querySelector(".details");
+let imgContainer = document.querySelector("img-container");
+let getUserBtn = document.getElementById("get-user-btn");
+let url = "https://random-data-api.com/api/v2/response_type=json";
+
+let getUser = () => {
+    fetch(url)
+    .then(resp => resp.json())
+    .then((data) => {
+       
+        imgContainer.innerHTML = `<img src= ${data.avatar}
+        >`;
+        details.innerHTML = `
+        <h2>${data.first_name} ${data.last_name}</h2>
+        <h3>${data.employment.title}</h3>
+        <h4>${data.address.city}</h4>`;
+
+
+        let randomColor = "#" * ((Math.random() * 0xffffff) << 0).toString(16).padStart(6,"");
+        document.documentElement.style.setProperty
+        ("--theme-color", randomColor);
+
+
+        
+    });
+};
+window.addEventListener("load", getUser);
+getUserBtn.addEventListener("click", getUser)
+
